7bf94c5516e9c47a4a984a682899caf9c49cde52
dxj <1718874198@qq.com>
2023-09-23 10:30:41 +0800

    [build]vite开发环境搭建

---
+++ vite.config.ts
@@ -0,0 +1,32 @@
+import {createVuePlugin} from "vite-plugin-vue2";
+import commonjs from "vite-plugin-commonjs";
+import {defineConfig} from "vite";
+import path from "path";
+
+export default defineConfig({
+    plugins: [
+        commonjs(),
+        createVuePlugin(),
+    ],
+    define: {
+        global: "globalThis",
+        process: process
+    },
+    resolve: {
+        alias: {
+            "@": path.resolve(__dirname, "src"),
+            fs: "rollup-plugin-node-polyfills/polyfills/empty",
+            net: "rollup-plugin-node-polyfills/polyfills/empty",
+            dns: "rollup-plugin-node-polyfills/polyfills/empty",
+            tls: "rollup-plugin-node-polyfills/polyfills/empty",
+            dgram: "rollup-plugin-node-polyfills/polyfills/empty",
+            stream: "rollup-plugin-node-polyfills/polyfills/stream",
+            process: "rollup-plugin-node-polyfills/polyfills/process-es6",
+            assert: "rollup-plugin-node-polyfills/polyfills/assert",
+            path: "rollup-plugin-node-polyfills/polyfills/path",
+            os: "rollup-plugin-node-polyfills/polyfills/os",
+            http: "rollup-plugin-node-polyfills/polyfills/http",
+            events: "rollup-plugin-node-polyfills/polyfills/events",
+        }
+    },
+});
